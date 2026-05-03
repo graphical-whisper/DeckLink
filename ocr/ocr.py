a21875e94bb36028c5594b21c5bc7d5d2914fc35
graphical-whisper <raulsantihallado@gmail.com>
2026-05-03 14:34:35 -0500

    modyfying ocr

--- ocr/ocr.py
+++ ocr/ocr.py
@@ -34,10 +34,11 @@ def extraer_numero_focalizado(img):
     # 1. Nuevas coordenadas estimadas para esta fotografía
     # Ajustado para considerar el fondo blanco y el soporte
     # 1. Nuevas coordenadas calibradas
-    y_inicio = int(alto * 0.865)  # Se desplaza hacia abajo para omitir el texto superior
-    y_fin = int(alto * 0.91)      # Se desplaza el límite inferior para no cortar la base de los números
-    x_inicio = int(ancho * 0.18)  # Se mueve a la izquierda para dar margen al "0"
-    x_fin = int(ancho * 0.31)     # Se recorta la derecha para eliminar las estrellas
+   # 1. Coordenadas de aislamiento estricto
+    y_inicio = int(alto * 0.87)   # Ajuste superior para omitir la franja amarilla
+    y_fin = int(alto * 0.90)      # Ajuste inferior para eliminar la textura gris base
+    x_inicio = int(ancho * 0.22)  # Desplazamiento a la derecha para omitir el recuadro "EN"
+    x_fin = int(ancho * 0.29)     # Recorte izquierdo para excluir la estrella
     
     recorte = img[y_inicio:y_fin, x_inicio:x_fin]
     
